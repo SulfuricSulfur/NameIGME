016b6ac88e518b21292759c7bb62108c2c4b1634
sicariusSummoned <jxt1607@g.rit.edu>
2015-11-06 21:57:47 -0500

    Added more to platform

--- MarketGame1/Platform.cs
+++ MarketGame1/Platform.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
 
 namespace MarketGame1
 {
     class Platform
     {
+        private Rectangle platRect;
+
+        public Platform(int x, int y, int w, int h)
+        {
+            platRect = new Rectangle(x, y, w, h);
+        }
     }
 }
