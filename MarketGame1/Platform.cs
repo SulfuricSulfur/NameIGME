6c1a177bb79d9494a27a226d1af74d57f211f66c
sicariusSummoned <jxt1607@g.rit.edu>
2015-11-06 21:32:19 -0500

    Added platform class and constructor.

---
+++ MarketGame1/Platform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarketGame1
+{
+    class Platform
+    {
+        int x;
+        int y;
+        int width;
+        int height;
+
+        public Platform(int xPos,int yPos, int widthSet, int heightSet)
+        {
+            x = xPos;
+            y = yPos;
+            width = widthSet;
+            height = heightSet;
+        }
+
+
+
+
+
+    }
+}
