1b33db76eb523c0a9762188a09c3e1b4973ed1b0
sicariusSummoned <jxt1607@g.rit.edu>
2015-11-06 21:55:53 -0500

    Platform

--- MarketGame1/Platform.cs
+++ MarketGame1/Platform.cs
@@ -5,27 +5,7 @@ using System.Text;
 
 namespace MarketGame1
 {
-    /// <summary>
-    /// This class contains platforms, platforms can be stood upon and have a position. Set their rect to their width and height at the position given.
-    /// </summary>
     class Platform
     {
-        int x;
-        int y;
-        int width;
-        int height;
-
-        public Platform(int xPos,int yPos, int widthSet, int heightSet)
-        {
-            x = xPos;
-            y = yPos;
-            width = widthSet;
-            height = heightSet;
-        }
-
-
-
-
-
     }
 }
