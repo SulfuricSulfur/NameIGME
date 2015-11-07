3980a9bcadc820cf8862dbf3c57ac787a1ff4227
sankethcb <sankethcb@gmail.com>
2015-11-06 20:58:14 -0500

    Added rest of the fruit classes

---
+++ MarketGame1/Grape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarketGame1
+{
+    public class Grape :Fruits
+    {
+        public Grape()
+        {
+            this.weight = 2;
+            this.name = "Grape";
+            this.pointvalue = 12;
+        }
+
+    }
+}
