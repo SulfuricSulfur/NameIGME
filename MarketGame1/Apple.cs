1a44ecab794ac6a2168419a38310505d25d8f90d
sankethcb <sankethcb@gmail.com>
2015-11-06 20:38:51 -0500

    Added 2 classes

---
+++ MarketGame1/Apple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarketGame1
+{
+    public class Apple :Fruits
+    {
+
+        
+    }
+}
---
+++ MarketGame1/Fruits.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarketGame1
+{
+    public class Fruits
+    {
+        private double weight;
+        private string name;
+        private int pointvalue;
+
+        public int PointValue
+        {
+            get { return pointvalue; }
+            set { pointvalue = value; }
+        }
+        
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+    }
+}
